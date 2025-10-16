9f211c4153f104bcc0c2d7fa5d885654ecf22681
chinnabut20 <ckartckart@gmail.com>
2025-10-16 16:02:19 +0700

    update

--- backend/main.py
+++ backend/main.py
@@ -77,27 +77,27 @@ def load_all_models():
                 print(f"⚠️ Missing files for {p}")
                 continue
 
-            # lstm_models[p] = load_model(
-            #     model_path,
-            #     custom_objects={
-            #         "mse": MeanSquaredError(),
-            #         "mean_squared_error": MSE(),
-            #         "MeanSquaredError": MSE()
-            #     }
-            # )
-
-
-            # เพิ่มใน load_all_models():
             lstm_models[p] = load_model(
                 model_path,
-                compile=False,  # ❗ ปิดการ compile ตอนโหลด
                 custom_objects={
                     "mse": MeanSquaredError(),
                     "mean_squared_error": MSE(),
-                    "MeanSquaredError": MSE(),
-                    "InputLayer": layers.InputLayer  # ป้องกัน error 'batch_shape'
+                    "MeanSquaredError": MSE()
                 }
             )
+
+
+            # เพิ่มใน load_all_models():
+            # lstm_models[p] = load_model(
+            #     model_path,
+            #     compile=False,  # ❗ ปิดการ compile ตอนโหลด
+            #     custom_objects={
+            #         "mse": MeanSquaredError(),
+            #         "mean_squared_error": MSE(),
+            #         "MeanSquaredError": MSE(),
+            #         "InputLayer": layers.InputLayer  # ป้องกัน error 'batch_shape'
+            #     }
+            # )
 
             scalers[p] = joblib.load(scaler_path)
             with open(config_path, "rb") as f:
